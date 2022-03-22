edfd52fbf5d2b4c9c7686c15458ecc6825497de9
tiou <dourokinga@gmail.com>
2022-03-22 11:43:16 +0800

    fix: fullscreen fix darwin only

--- init.el
+++ init.el
@@ -770,16 +770,18 @@
 
 ;; 规避 macOS child-frame 全屏黑屏
 ;; https://emacs-china.org/t/mac/11848/8?u=douo
-(if (featurep 'cocoa)
-    (progn
-      (setq ns-use-native-fullscreen nil)
-      (setq ns-use-fullscreen-animation nil)
+(with-system darwin
+  (if (featurep 'cocoa)
+      (progn
+        (setq ns-use-native-fullscreen nil)
+        (setq ns-use-fullscreen-animation nil)
 
-      (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+        (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
 
-      (run-at-time "2sec" nil
-                   (lambda ()
-                     (toggle-frame-fullscreen)
-                     )))
-  (require 'fullscreen)
-  (fullscreen))
+        (run-at-time "2sec" nil
+                     (lambda ()
+                       (toggle-frame-fullscreen)
+                       )))
+    (require 'fullscreen)
+    (fullscreen))
+  )
