bd081282f9790d639693772766c8a47696924ed3
douo <dourokinga@gmail.com>
2023-02-22 17:27:28 +0800

    eglot: clangd

--- tui.el
+++ tui.el
@@ -16,10 +16,23 @@
   :ensure t
   :config
   (add-to-list
-   'eglot-server-programs `(python-mode . (lambda(a)
-                                            `(,(executable-find "python") "-m" "pyright.langserver" "--stdio")
-                                            )))
-  (add-hook pyvenv-post-activate-hooks douo/update_eglot_pyright_configuraton)
+   'eglot-server-programs
+   `(python-mode . (lambda(a)
+                     `(,(executable-find "python") "-m" "pyright.langserver" "--stdio"))))
+   (add-to-list
+    'eglot-server-programs
+    '((c-mode c++-mode)
+      . ("clangd"
+         "-j=12"
+         "--malloc-trim"
+         "--background-index"
+         "--clang-tidy"
+         "--cross-file-rename"
+         "--completion-style=detailed"
+         "--pch-storage=memory"
+         "--function-arg-placeholders"
+         "--header-insertion=iwyu")))
+  (add-hook 'pyvenv-post-activate-hooks 'douo/update_eglot_pyright_configuraton)
   :hook
   (python-mode . douo/update_eglot_pyright_configuraton)
   (python-mode . eglot-ensure)
