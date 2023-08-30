b71954cded70a7b25ece38a2b0e70beec46554f5
tiou <dourokinga@gmail.com>
2023-08-30 17:41:21 +0800

    misc: clock show second & disable auto-fullscreen

--- init.el
+++ init.el
@@ -1351,7 +1351,15 @@
 
 
 ;; 在 mode-line 显示时间
-(display-time-mode)
+(use-package time
+  :custom
+  (display-time-format "%H:%M:%S")
+  (display-time-24hr-format 1)
+  (display-time-interval 1)
+  :config
+  (display-time-mode 1)
+  )
+
 
 ;; tui/gui 切换不同配置，主要是切换 lsp-bridge 和 eglot
 (if (display-graphic-p)
@@ -1361,24 +1369,26 @@
   )
 
 ;; macOS Fix
-(with-system darwin
-  ;; 规避 macOS child-frame 全屏黑屏
-  ;; https://emacs-china.org/t/mac/11848/8
-  (if (featurep 'cocoa)
-      (progn
-        (setq ns-use-native-fullscreen nil)
-        (setq ns-use-fullscreen-animation nil)
+;; FIXME to_be_deleted
+;; 交给 wm(yabai) 处理
+;; (with-system darwin
+;;   ;; 规避 macOS child-frame 全屏黑屏
+;;   ;; https://emacs-china.org/t/mac/11848/8
+;;   (if (featurep 'cocoa)
+;;       (progn
+;;         (setq ns-use-native-fullscreen nil)
+;;         (setq ns-use-fullscreen-animation nil)
 
-        (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+;;         (set-frame-parameter (selected-frame) 'fullscreen 'maximized)
+;;         (menu-bar-mode t)
+;;         (run-at-time "2sec" nil
+;;                      (lambda ()
+;;                        (toggle-frame-fullscreen)
+;;                        )))
+;;     (require 'fullscreen)
+;;     (fullscreen))
 
-        (run-at-time "2sec" nil
-                     (lambda ()
-                       (toggle-frame-fullscreen)
-                       )))
-    (require 'fullscreen)
-    (fullscreen))
-
-  )
+;;   )
 
 ;; * copilot
 (setq douo/copilot-provider `copilot)
