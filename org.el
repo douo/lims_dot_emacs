6bdab88a12a610960e6df8985da79bc00b597e6a
tiou <dourokinga@gmail.com>
2023-09-11 17:56:23 +0800

    org: add note

--- org.el
+++ org.el
@@ -295,3 +295,26 @@ Throw an error when not in a list."
 (use-package org-checklist
   :straight  '(org-contrib :includes org-checklist)
   )
+
+
+(use-package org-roam
+  :straight t
+  :custom
+  (org-roam-directory (file-truename (concat douo/writing-home "/_roam/")))
+  :bind
+  ("C-c n l" . org-roam-buffer-toggle)
+  ("C-c n f" . org-roam-node-find)
+  ("C-c n g" . org-roam-graph)
+  ("C-c n i" . org-roam-node-insert)
+  ("C-c n c" . org-roam-capture)
+  ;; Dailies
+  ("C-c n j" . org-roam-dailies-capture-today)
+  :config
+  ;; If you're using a vertical completion framework, you might want a more informative completion interface
+  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol))
+
+(use-package org-noter
+  :straight t)
