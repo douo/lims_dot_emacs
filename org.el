d39764e976a0fad8c7ec52227e60e6972db8ba27
douo <dourokinga@gmail.com>
2023-09-14 15:11:57 +0800

    org: fix org-roam-procotol in linux

--- org.el
+++ org.el
@@ -314,8 +314,8 @@ Throw an error when not in a list."
   ;; If you're using a vertical completion framework, you might want a more informative completion interface
   (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
-  ;; If using org-roam-protocol
-  (require 'org-roam-protocol))
+  )
+(require 'org-roam-protocol)
 
 (use-package org-noter
   :straight t)
