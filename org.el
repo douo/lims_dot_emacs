929f576f5f66ee6f47efcd55101ef8bee1d7add8
tiou <dourokinga@gmail.com>
2023-09-14 20:15:52 +0800

    roam: consult-org-roam

--- org.el
+++ org.el
@@ -304,7 +304,7 @@ Throw an error when not in a list."
   (org-roam-dailies-directory "quick/")
   :bind
   ("C-c n l" . org-roam-buffer-toggle)
-  ("C-c n f" . org-roam-node-find)
+    ;; ("C-c n f" . org-roam-node-find) ;; 由 `consult-org-roam-file-find'  代替
   ("C-c n g" . org-roam-graph)
   ("C-c n i" . org-roam-node-insert)
   ("C-c n c" . org-roam-capture)
@@ -316,6 +316,36 @@ Throw an error when not in a list."
   (org-roam-db-autosync-mode)
   )
 (require 'org-roam-protocol)
+
+
+(use-package consult-org-roam
+   :straight t
+   :after org-roam
+   :init
+   (require 'consult-org-roam)
+   ;; Activate the minor mode
+   (consult-org-roam-mode 1)
+   :custom
+   ;; Use `ripgrep' for searching with `consult-org-roam-search'
+   (consult-org-roam-grep-func #'consult-ripgrep)
+   ;; Configure a custom narrow key for `consult-buffer'
+   (consult-org-roam-buffer-narrow-key ?r)
+   ;; Display org-roam buffers right after non-org-roam buffers
+   ;; in consult-buffer (and not down at the bottom)
+   (consult-org-roam-buffer-after-buffers t)
+   :config
+   ;; Eventually suppress previewing for certain functions
+   (consult-customize
+    consult-org-roam-forward-links
+    :preview-key (kbd "M-."))
+   :bind
+   ;; Define some convenient keybindings as an addition
+   ("C-c n f" . consult-org-roam-file-find)
+   ("C-c n b" . consult-org-roam-backlinks)
+   ("C-c n l" . consult-org-roam-forward-links)
+   ("C-c n r" . consult-org-roam-search) ;; TODO 可以整合 `deft' 的功能
+   )
+
 
 (use-package org-noter
   :straight t)
