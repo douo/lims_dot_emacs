c67bdd8a3a8f2c654b5a82dd0b75675b3cb82e82
tiou <dourokinga@gmail.com>
2024-04-25 00:54:18 +0800

    refactor: move llm relative package to one place

--- init.el
+++ init.el
@@ -322,65 +322,7 @@
   :straight t
   :bind (("M-g t" . git-timemachine)))
 
-;; gpt
-(use-package gptel
-  :straight t)
 
-;; llm
-(use-package ellama  ;; 依赖 https://github.com/ahyatt/llm
-  :straight t
-  :init
-  ;; setup key bindings
-  (setopt ellama-keymap-prefix "s-o")
-  ;; language you want ellama to translate to
-  (setopt ellama-language "Chinese")
-  ;; could be llm-openai for example
-  (require 'llm-ollama)
-  (setopt ellama-provider
-	  (make-llm-ollama
-	   ;; this model should be pulled to use it
-	   ;; value should be the same as you print in terminal during pull
-           :host "p44.zero"
-	   :chat-model "llama3"
-	   :embedding-model "llama3"))
-  ;; Predefined llm providers for interactive switching.
-  ;; You shouldn't add ollama providers here - it can be selected interactively
-  ;; without it. It is just example.
-  (setopt ellama-providers
-	  '(("llama3" . (make-llm-ollama
-                            :host "p44.zero"
-		            :chat-model "llama3"
-			    :embedding-model "llama3"))
-	    ("dolphin-llama3" . (make-llm-ollama
-                                 :host "p44.zero"
-			         :chat-model "dolphin-llama3:8b-v2.9-fp16"
-			         :embedding-model "dolphin-llama3:8b-v2.9-fp16"))
-	    ("qwen" . (make-llm-ollama
-                       :host "p44.zero"
-		       :chat-model "qwen:32b"
-		       :embedding-model "qwen:32b"))))
-  ;; Naming new sessions with llm
-  (setopt ellama-naming-provider ellama-provider)
-  (setopt ellama-naming-scheme 'ellama-generate-name-by-llm)
-  ;; Translation llm provider
-  (setopt ellama-translation-provider ellama-provider))
-
-(use-package magit-gptcommit
-  :straight t
-  :demand t
-  :after gptel magit
-  :config
-  ;; Enable magit-gptcommit-mode to watch staged changes and generate commit message automatically in magit status buffer
-  ;; This mode is optional, you can also use `magit-gptcommit-generate' to generate commit message manually
-  ;; `magit-gptcommit-generate' should only execute on magit status buffer currently
-  ;; (magit-gptcommit-mode 1)
-
-  ;; Add gptcommit transient commands to `magit-commit'
-  ;; Eval (transient-remove-suffix 'magit-commit '(1 -1)) to remove gptcommit transient commands
-  (magit-gptcommit-status-buffer-setup)
-  :bind (:map git-commit-mode-map
-              ("C-c C-g" . magit-gptcommit-commit-accept))
-  )
 
 ;; rg
 (use-package rg
@@ -1774,6 +1716,7 @@
 
 ;; begin_other_init
 (require 'init-org)
+(require 'init-llm)
 ;; tui/gui 切换不同配置，+主要是切换 lsp-bridge 和 eglot+
 (if (display-graphic-p)
     (require 'init-gui)
