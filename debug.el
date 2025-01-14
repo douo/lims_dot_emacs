b07c733f458d396070b97d04feedd31637e7abcf
tiou <dourokinga@gmail.com>
2025-01-14 21:50:26 +0800

    master add

---
+++ debug.el
@@ -0,0 +1,21 @@
+;; 用于快速调试配置
+;; emacs  -Q -l ~/.emacs.d/debug.el ~/.emacs.d/debug.el
+
+;;; 包管理器初始化
+(setq package-enable-at-startup nil)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 6))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;;; 调试代码写在这下面
+
